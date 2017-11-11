c2ccbfef6d66ccd86a44d6891a5c0ce1aee762d3
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-11 13:02:12 +0100

    Create index.php
    Index de la app.

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+// file: index.php
+
+
+define("DEFAULT_CONTROLLER", "login");
+
+define("DEFAULT_ACTION", "index");
+
+function run() {
+	try {
+		if (!isset($_GET["controller"])) {
+			$_GET["controller"] = DEFAULT_CONTROLLER;
+		}
+
+		if (!isset($_GET["action"])) {
+			$_GET["action"] = DEFAULT_ACTION;
+		}
+
+		$controller = loadController($_GET["controller"]);
+
+		$actionName = $_GET["action"];
+		$controller->$actionName();
+	} catch(Exception $ex) {
+
+		die("An exception occured!!!!!".$ex->getMessage());
+	}
+}
+
+
+function loadController($controllerName) {
+	$controllerClassName = getControllerClassName($controllerName);
+
+	require_once(__DIR__."/controller/".$controllerClassName.".php");
+	return new $controllerClassName();
+}
+
+
+function getControllerClassName($controllerName) {
+	return strToUpper(substr($controllerName, 0, 1)).substr($controllerName, 1)."Controller";
+}
+
+
+run();
+
+?>
