878c86cb655e82aeeb9ad179650ccbb0cc08e472
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-11 13:10:26 +0100

    Create default.php

---
+++ view/layouts/default.php
@@ -0,0 +1,45 @@
+<?php
+//file: view/layouts/default.php
+
+$view = ViewManager::getInstance();
+$currentuser = $view->getVariable("currentusername");
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<title><?= $view->getVariable("title", "WorldNote®") ?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<!-- enable ji18n() javascript function to translate inside your scripts -->
+	<script src="index.php?controller=language&amp;action=i18njs">
+	</script>
+	<?= $view->getFragment("css") ?>
+	<?= $view->getFragment("javascript") ?>
+</head>
+<body>
+
+	<header>
+		<ul class="nav">
+			<li><a><?= i18n("Language:") ?></a></li>
+			<?php
+			include(__DIR__."/language_select_element.php");
+			?>
+			<li><a href="#">Contacto</a></li>
+		</ul>
+	</header>
+
+	<main>
+		<div id="flash">
+			<?= $view->popFlash() ?>
+		</div>
+
+		<?= $view->getFragment(ViewManager::DEFAULT_FRAGMENT) ?>
+	</main>
+
+	<footer>
+
+	</footer>
+
+</body>
+</html>
