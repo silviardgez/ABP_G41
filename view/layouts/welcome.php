423e143177257310eb6eabc84b2c57ab007c1000
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-12 21:57:20 +0100

    Update welcome.php

--- view/layouts/welcome.php
+++ view/layouts/welcome.php
@@ -2,6 +2,7 @@
 // file: view/layouts/welcome.php
 
 $view = ViewManager::getInstance();
+$type = $view->getVariable("type2"); 
 
 ?><!DOCTYPE html>
 <html>
@@ -13,24 +14,113 @@ $view = ViewManager::getInstance();
 	<?= $view->getFragment("css") ?>
 	<?= $view->getFragment("javascript") ?>
 </head>
+
 <body>
 	<header>
 		<?php if (isset($_SESSION["currentuser"])): ?>
-			<ul class="nav">
-				<li><a href="index.php?controller=login&amp;action=home"><?= i18n("Start")?></a></li>
-				<li><a><?=i18n("User")?></a>
-					<ul>
-						<li><a><?=i18n("Connected as:")?> <?= $_SESSION["currentuser"]?></a></li>
-					</ul>
-				</li>
-				<li><a><?=i18n("Language")?></a>
-					<ul>
-						<li><a href="index.php?controller=language&amp;action=change&amp;lang=es"><?= i18n("Spanish") ?></a></li>
-						<li><a href="index.php?controller=language&amp;action=change&amp;lang=en"><?= i18n("English") ?></a></li>
-					</ul>
-				</li>
-				<li><a id="logout" href="index.php?controller=users&amp;action=logout"><?= i18n("Logout")?></a></li>
-			</ul>
+			<?php if ($type == "admin"): ?>
+				<ul class="navAdmin">
+					<li><a href="index.php?controller=login&amp;action=home"><?= i18n("Start")?></a></li>
+					<li><a><?=i18n("User")?></a>
+						<ul>
+							<li><a><?=i18n("Connected as:")?> <?= $_SESSION["currentuser"]?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Administration")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Notifications")?></a></li>
+							<li><a href=""><?=i18n("Users")?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Statistics")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Athletes")?></a></li>
+							<li><a href=""><?=i18n("Activities")?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Fitness")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Activities")?></a></li>
+							<li><a href=""><?=i18n("Reservations")?></a></li>
+							<li><a href=""><?=i18n("Assistance")?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Activities")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Exercises")?></a></li>
+							<li><a href=""><?=i18n("Workouts")?></a></li>
+							<li><a href=""><?=i18n("Tables")?></a></li>
+						</ul>
+					</li>
+					<li><a><?=i18n("Language")?></a>
+						<ul>
+							<li><a href="index.php?controller=language&amp;action=change&amp;lang=es"><?= i18n("Spanish") ?></a></li>
+							<li><a href="index.php?controller=language&amp;action=change&amp;lang=en"><?= i18n("English") ?></a></li>
+						</ul>
+					</li>
+					<li><a id="logout" href="index.php?controller=users&amp;action=logout"><?= i18n("Logout")?></a></li>
+				</ul>
+			<?php endif ?>
+			<?php if ($type == "entrenador"): ?>
+				<ul class="navEn">
+					<li><a href="index.php?controller=login&amp;action=home"><?= i18n("Start")?></a></li>
+					<li><a><?=i18n("User")?></a>
+						<ul>
+							<li><a><?=i18n("Connected as:")?> <?= $_SESSION["currentuser"]?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Statistics")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Athletes")?></a></li>
+							<li><a href=""><?=i18n("Activities")?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Fitness")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Activities")?></a></li>
+							<li><a href=""><?=i18n("Reservations")?></a></li>
+							<li><a href=""><?=i18n("Assistance")?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Activities")?></a>
+						<ul>
+							<li><a href=""><?=i18n("Exercises")?></a></li>
+							<li><a href=""><?=i18n("Workouts")?></a></li>
+							<li><a href=""><?=i18n("Tables")?></a></li>
+						</ul>
+					</li>
+					<li><a><?=i18n("Language")?></a>
+						<ul>
+							<li><a href="index.php?controller=language&amp;action=change&amp;lang=es"><?= i18n("Spanish") ?></a></li>
+							<li><a href="index.php?controller=language&amp;action=change&amp;lang=en"><?= i18n("English") ?></a></li>
+						</ul>
+					</li>
+					<li><a id="logout" href="index.php?controller=users&amp;action=logout"><?= i18n("Logout")?></a></li>
+				</ul>
+			<?php endif ?>
+			<?php if ($type == "deportista"): ?>
+				<ul class="navDep">
+					<li><a href="index.php?controller=login&amp;action=home"><?= i18n("Start")?></a></li>
+					<li><a><?=i18n("Profile")?></a>
+						<ul>
+							<li><a><?=i18n("Connected as:")?> <?= $_SESSION["currentuser"]?></a></li>
+							<li><a href=""><?=i18n("Notifications")?></a></li>
+							<li><a href=""><?=i18n("Sesions")?></a></li>
+						</ul>
+					</li>
+					<li><a href=""><?=i18n("Workouts")?></a></li>
+					<li><a href=""><?=i18n("Exercises")?></a></li>
+					<li><a href=""><?=i18n("Tables")?></a></li>
+					<li><a href=""><?=i18n("Services")?></a></li>
+					<li><a><?=i18n("Language")?></a>
+						<ul>
+							<li><a href="index.php?controller=language&amp;action=change&amp;lang=es"><?= i18n("Spanish") ?></a></li>
+							<li><a href="index.php?controller=language&amp;action=change&amp;lang=en"><?= i18n("English") ?></a></li>
+						</ul>
+					</li>
+					<li><a id="logout" href="index.php?controller=users&amp;action=logout"><?= i18n("Logout")?></a></li>
+				</ul>
+			<?php endif ?>
 		<?php endif ?>
 	</header>
 	<main>
