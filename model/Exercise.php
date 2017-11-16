ded0a5067b80b6b14e9aee022f08d22c685bf7f7
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-16 19:12:35 +0100

    Add files via upload

---
+++ model/Exercise.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once(__DIR__."/../core/ValidationException.php");
+require_once(__DIR__."/../model/ExerciseMapper.php");
+
+class Exercise{
+
+	private $id_exercise;
+	private $name;
+	private $type;
+	private $image;
+	private $video;
+
+	public function __construct($id_exercise=NULL, $name=NULL, $type=NULL, $image=NULL, $video=NULL){
+		$this->id_exercise = $id_exercise;
+		$this->type = $type;
+		$this->name = $name;
+		$this->image = $image;
+		$this->video = $video;
+	}
+
+	//Setters
+	public function setId($id_exercise){
+		$this->id_exercise = $id_exercise;
+	}
+
+	public function setImage($image){
+		$this->image = $image;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function setType($type){
+		$this->type = $type;
+	}
+
+	public function setVideo($video){
+		$this->video = $video;
+	}
+
+	//Validations for add exercises
+	public function ValidRegister(){
+		$errors = array();
+		if (strlen($this->name) < 5 ) {
+			$errors["name"] = "Name must be at least 5 characters length";
+		}
+		if (sizeof($errors)>0){
+			throw new ValidationException($errors, "Exercise is not valid");
+		}
+	}
+
+	//Getters
+	public function getId(){
+		return $this->id_exercise;
+	}
+
+	public function getType(){
+		return $this->type;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function getImage(){
+		return $this->image;
+	}
+
+	public function getVideo(){
+		return $this->video;
+	}
+
+	//Update Validate
+	public function checkIsValidForUpdate() {
+		$errors = array();
+		
+		if (!isset($this->name)) {
+			$errors["name"] = "Name is mandatory";
+		}
+		if (!isset($this->id_exercise)) {
+			$errors["id"] = "Id is mandatory";
+		}
+	}
+	
+}
+
+?>
---
+++ model/ExerciseMapper.php
@@ -0,0 +1,65 @@
+<?php
+// file: model/ExerciseMapper.php
+
+require_once(__DIR__."/../core/PDOConnection.php");
+
+
+class ExerciseMapper {
+
+	private $db;
+
+	public function __construct() {
+		$this->db = PDOConnection::getInstance();
+	}
+	
+
+	public function showAllExercises(){
+		$stmt = $this->db->prepare("SELECT * FROM EJERCICIO");
+		$stmt->execute();
+		$exercises_db = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+		$exercise = array();
+		//var_dump($exercises_db);
+
+		foreach ($exercises_db as $exercises) {
+			array_push($exercise, new Exercise($exercises["ID_EJERCICIO"], $exercises["NOMBRE"], $exercises["TIPO"], $exercises["IMAGEN"], $exercises["VIDEO"]));
+		}
+
+		return $exercise;
+
+	}
+
+	public function findExerciseById($id){
+		$stmt = $this->db->prepare("SELECT * FROM EJERCICIO WHERE ID_EJERCICIO=?");
+		$stmt->execute(array($id));
+		$exercise = $stmt->fetch(PDO::FETCH_ASSOC);
+		
+		if($exercise != null) {
+			return new Exercise(
+				$exercise["ID_EJERCICIO"],
+				$exercise["NOMBRE"],
+				$exercise["TIPO"],
+				$exercise["IMAGEN"],
+				$exercise["VIDEO"]);
+		} else {
+			return NULL;
+		}
+	}
+
+	public function update(Exercise $exercise){
+		$stmt = $this->db->prepare("UPDATE EJERCICIO set NOMBRE=?, TIPO=?, IMAGEN=?, VIDEO=? where ID_EJERCICIO=?");
+		$stmt->execute(array($exercise->getName(), $exercise->getType(), $exercise->getImage(), $exercise->getVideo(), $exercise->getId()));
+	}
+
+	public function delete($exercise){
+		$stmt = $this->db->prepare("DELETE from EJERCICIO WHERE ID_EJERCICIO=?");
+		$stmt->execute(array($exercise->getId()));
+	}
+
+	public function add(Exercise $exercise){
+		$stmt = $this->db->prepare("INSERT INTO EJERCICIO(NOMBRE,TIPO,IMAGEN,VIDEO) values (?,?,?,?)");
+		$stmt->execute(array($exercise->getName(), $exercise->getType(), $exercise->getImage(), $exercise->getVideo()));
+		return $this->db->lastInsertId();
+	}
+
+}
