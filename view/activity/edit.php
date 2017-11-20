331c17b415383d39e9b6e5539f6c83658a7db516
Silvia Rodríguez <silviardguez@gmail.com>
2017-11-20 15:27:56 +0100

    Added messages in messages_es

--- view/activity/edit.php
+++ view/activity/edit.php
@@ -16,6 +16,16 @@ $view->setVariable("title", "Edit Activity");
 		<div class="home2">
 			<form action="index.php?controller=activity&amp;action=edit" method="POST">
 				<br><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?><input type="text" name="nombre" value="<?=$activity->getActivityName()?>"> 
+				<br><?=i18n("Day")?>:<?= isset($errors["day"])?i18n($errors["day"]):"" ?>
+				<select name="tipo">
+					<option value="LUNES" selected="selected"><?=i18n("Monday")?></option>
+					<option value="MARTES"><?=i18n("Tuesday")?></option>
+					<option value="MIERCOLES"><?=i18n("Wednesday")?></option>
+					<option value="JUEVES"><?=i18n("Thursday")?></option>
+					<option value="VIERNES"><?=i18n("Friday")?></option>
+					<option value="SABADO"><?=i18n("Saturday")?></option>
+					<option value="DOMINGO"><?=i18n("Sunday")?></option>
+				</select>
 
 
 				<button type="submit" name="submit"><?=i18n("Send")?></button>
