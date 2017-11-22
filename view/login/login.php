8cbe5c9aad7173e6cb010199332bff968e2fa1f7
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-22 09:48:03 +0100

    Update login.php

--- view/login/login.php
+++ view/login/login.php
@@ -3,11 +3,12 @@
 
 require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
+$view->setLayout("default");
 $view->setVariable("title", "Login");
 $errors = $view->getVariable("errors");
 ?>
 
-<?= isset($errors["general"])?$errors["general"]:"" ?>
+
 <div class="recuadro">
 	<div id="formulario">
 
@@ -15,11 +16,21 @@ $errors = $view->getVariable("errors");
 			<form action="index.php?controller=users&amp;action=login" method="POST">
 				<?= i18n("DNI")?>: <input class="typetext" type="text" name="username">
 				<?= i18n("Password")?>: <input class="typetext" type="password" name="passwd">
+
 				<button type="submit" name="submit" class="cancel"><i class="button fa fa-sign-in"></i></button>
 			</form>
+			<?= i18n("Forgot your password?")?> <a href="index.php?controller=users&amp;action=recover"><?= i18n("Click here!")?></a>
 		</div>
 	</div>
 </div>
+<!-- flash message -->
+		<div class="flash">
+			<?php if(isset($errors["general"])){
+				$acumulador = $errors["general"];
+				echo "<script>alert ('$acumulador');</script>";
+			} ?>
+			
+		</div>
 
 <?php $view->moveToFragment("css");?>
 <link rel="stylesheet" type="text/css" src="css/style.css">
