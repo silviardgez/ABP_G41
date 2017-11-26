9e78a56ebe02462a731af4731248a7a01e3de170
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 12:02:26 +0100

    Update viewcurrent.php

--- view/users/viewcurrent.php
+++ view/users/viewcurrent.php
@@ -6,7 +6,7 @@ $user = $view->getVariable("user");
 $view->setVariable("title", "View CurrentUser");
 ?>
 
-	<h1>Ver Perfil</h1>
+	<h1><?=i18n("View Profile")?></h1>
 	<form action="index.php?controller=users&amp;action=editcurrent&amp;dni=<?= $_SESSION['currentuser'] ?>" method="POST" class="form-horizontal col-md-12" >
 		<br><div class="form-group">
 			<label class="col-lg-6 control-label"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
