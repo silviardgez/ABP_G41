7332b6e3404e1e6ab97dcdea9d048dc7eea62d57
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:41:59 +0100

    Update add.php

--- view/exercises/add.php
+++ view/exercises/add.php
@@ -7,27 +7,39 @@ $view->setVariable("title", "Add Exercise");
 $errors = $view->getVariable("errors");
 ?>
 
-<div class="recuadro">
-	<div id="formulario">
-
-		<div class="home2">
-			<form action="index.php?controller=exercises&amp;action=add" method="POST">
-				<br><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?><input type="text" name="name">  
-
-				<?=i18n("Type")?>:
-				<select name="type">
-					<option value="CARDIO"><?=i18n("Cardio")?></option>
-					<option value="MUSCULAR"><?=i18n("Muscular")?></option>
-					<option value="ESTIRAMIENTO"><?=i18n("Stretch")?></option>
-				</select>
-
-				<?=i18n("Image")?>:<input type="file" name="image"> 
-
-				<?=i18n("Video")?>:<input type="file" name="video">
-
-				<button type="submit" name="submit"><?=i18n("Send")?></button>
-
-			</form>
+<h1>Añadir Ejercicio</h1>
+<form action="index.php?controller=exercises&amp;action=add" method="POST" class="form-horizontal col-md-12" >
+	<br><div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+		<div class="col-lg-7">
+			<input type="text" name="nombre">
 		</div>
 	</div>
-</div>
+	<div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Type")?>:</label>
+		<div class="col-lg-7">
+			<select name="type">
+				<option value="CARDIO"><?=i18n("Cardio")?></option>
+				<option value="MUSCULAR"><?=i18n("Muscular")?></option>
+				<option value="ESTIRAMIENTO"><?=i18n("Stretch")?></option>
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Image")?>:</label>
+		<div class="col-lg-7">
+			<input type="file" name="image">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Video")?>:</label>
+		<div class="col-lg-7">
+			<input type="file" name="video">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-lg-offset-5 col-lg-7">
+			<button type="submit" name="submit"><?=i18n("Send")?></button>
+		</div>
+	</div>
+</form>
