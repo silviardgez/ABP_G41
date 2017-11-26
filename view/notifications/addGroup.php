f4a93027f56ddf4dc97efbc8d8cb4f170961d5c9
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:44:19 +0100

    Update addGroup.php

--- view/notifications/addGroup.php
+++ view/notifications/addGroup.php
@@ -6,21 +6,28 @@ $view->setVariable("title", "Add Group Notification");
 $id = $view->getVariable("id");
 ?>
 
-<div class="recuadro">
-	<div id="formulario">
-
-		<div class="home2">
-			<form action="index.php?controller=notifications&amp;action=add" method="POST">
-
-				<input type="hidden" name="id" value="<?= $id ?>">
-
-				<?= i18n("Subject")?>:<input type="text" name="subject"> 
-
-				<?=i18n("Message")?>:<input type="text" name="message">
-
-				<button type="submit" name="submit"><?=i18n("Send")?></button>
-
-			</form>
+<h1>Añadir Notificación Grupal</h1>
+<form action="index.php?controller=notifications&amp;action=add" method="POST" class="form-horizontal col-md-12" >
+	<br><div class="form-group">
+		<div class="col-lg-6">
+			<input type="hidden" name="id" value="<?= $id ?>">
 		</div>
 	</div>
-</div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Subject")?>:</label>
+		<div class="col-lg-6">
+			<input type="text" name="subject">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Message")?>:</label>
+		<div class="col-lg-6">
+			<textarea rows="3" name="message"></textarea>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-lg-offset-6 col-lg-6">
+			<button type="submit" name="submit" class="btn btn-default"><?=i18n("Send")?></button>
+		</div>
+	</div>
+</form>
