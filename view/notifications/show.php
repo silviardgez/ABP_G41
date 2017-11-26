0e21f87ee5d5bec8bc4e41d4c622aabacaa4a20d
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:44:34 +0100

    Update show.php

--- view/notifications/show.php
+++ view/notifications/show.php
@@ -9,29 +9,55 @@ $activities = $view->getVariable("activities");
 $id = $view->getVariable("id");
 ?>
 
-<div class="users">
-	<div class="margin">
-		<div class="home2">
-			<h1><?=i18n("Users")?></h1><br>
-			<?php foreach ($users as $user): ?>
-
-				<a href="index.php?controller=notifications&amp;action=add&amp;dni=<?= $user->getUsername() ?>&amp;email=<?= $user->getEmail() ?>"><?= htmlentities($user->getName()); echo ' '.htmlentities($user->getSurname()) ?></a><br><br>
-
-			<?php endforeach; ?>
-		</div>
+<div class="container">
+	<div class="table-responsive col-md-6">
+		<table class="table">
+			<thead>
+				<tr>
+					<th></th>
+					<th class="tittle"><?=i18n("Users")?></th>
+					<th></th>
+				</tr>
+				<tr class="active">
+					<th><?=i18n("Name")?></th>
+					<th><?=i18n("Surname")?></th>
+					<th><?=i18n("DNI")?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($users as $user): ?>
+						<tr class="success">
+							<td><a href="index.php?controller=notifications&amp;action=add&amp;dni=<?= $user->getUsername() ?>&amp;email=<?= $user->getEmail() ?>"><?= htmlentities($user->getName())?></a></td>
+							<td><?= htmlentities($user->getSurname()) ?></td>
+							<td><?= htmlentities($user->getUsername()) ?></td>
+						</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
 	</div>
-
-	<div class="margin">
-		<div class="home2">
-			<h1><?=i18n("Classes")?></h1><br>
-			<?php foreach ($activities as $activity): ?>
-
-				<a href="index.php?controller=notifications&amp;action=addGroup&amp;id=<?= $activity["ID_ACT"] ?>"><?= htmlentities($activity["NOMBRE"]);?></a><br><br>
-
-			<?php endforeach; ?>
-		</div>
+	<div class="table-responsive col-md-6">
+		<table class="table">
+			<thead>
+				<tr>
+					<th></th>
+					<th class="tittle"><?=i18n("Classes")?></th>
+					<th></th>
+				</tr>
+				<tr class="active">
+					<th><?=i18n("Name")?></th>
+					<th><?=i18n("Type")?></th>
+					<th><?=i18n("Day")?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($activities as $activity): ?>
+						<tr class="success">
+							<td><a href="index.php?controller=notifications&amp;action=addGroup&amp;id=<?= $activity["ID_ACT"] ?>"><?= htmlentities($activity["NOMBRE"])?></a></td>
+							<td><?= htmlentities($activity["TIPO"]) ?></td>
+							<td><?= htmlentities($activity["DIA"]) ?></td>
+						</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
 	</div>
-
 </div>
-
-
