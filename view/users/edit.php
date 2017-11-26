e5d94a97638f3d45d0d777c07dde3bd6d660adf8
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:45:16 +0100

    Update edit.php

--- view/users/edit.php
+++ view/users/edit.php
@@ -11,40 +11,88 @@ $view->setVariable("title", "Edit User");
 
 ?>
 
-<div class="recuadro">
-	<div id="formulario">
+<h1>Modificar Usuario</h1>
+<form action="index.php?controller=users&amp;action=edit" method="POST" class="form-horizontal col-md-12" >
+  <br><div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" name="nombre" value="<?=$user->getName()?>">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Surname")?>:<?= isset($errors["surname"])?i18n($errors["surname"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" name="apellidos" value="<?=$user->getSurname()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Date Born")?>:<?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" id="fecha" name="fechaNac" value="<?=$user->getDateBorn()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Email")?>:<?= isset($errors["email"])?i18n($errors["email"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="email" name="email" value="<?=$user->getEmail()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Telephone")?>:<?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" name="tel" value="<?=$user->getTlf()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("DNI")?>:<?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?></label>
+    <div class="col-lg-6">
+    	<input type="text" name="dni" value="<?=$user->getUsername()?>" readonly="readonly">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-lg-offset-6 col-lg-6">
+      <div class="checkbox">
+        <label>
+					<?php if($user->getAdmin() == 1){ ?>
+          	<input type="checkbox" name="administrador" value="1" checked="checked"> <?=i18n("Administrator")?>
+						<?php }else{ ?>
+							<input type="checkbox" name="administrador" value="1"> <?=i18n("Administrator")?>
+							<?php }?>
+        </label>
+      </div>
+    </div>
+  </div>
+	<div class="form-group">
+    <div class="col-lg-offset-6 col-lg-6">
+      <div class="checkbox">
+        <label>
+					<?php if($user->getCoach() == 1){ ?>
+          	<input type="checkbox" name="entrenador" value="1" checked="checked"> <?=i18n("Coach")?>
+						<?php }else{ ?>
+							<input type="checkbox" name="entrenador" value="1"> <?=i18n("Coach")?>
+							<?php }?>
+        </label>
+      </div>
+    </div>
+  </div>
+	<div class="form-group">
+    <div class="col-lg-offset-6 col-lg-6">
+      <div class="checkbox">
+        <label>
+					<?php if($user->getDeportist() == 1){ ?>
+          	<input type="checkbox" name="deportista" value="1" checked="checked"> <?=i18n("Athlete")?>
+						<?php }else{ ?>
+							<input type="checkbox" name="deportista" value="1"> <?=i18n("Athlete")?>
+							<?php }?>
+        </label>
+      </div>
+    </div>
+  </div>
 
-		<div class="home2">
-			<form action="index.php?controller=users&amp;action=edit" method="POST">
-				<br><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?><input type="text" name="nombre" value="<?=$user->getName()?>"> 
 
-				<?=i18n("Surname")?>:<?= isset($errors["surname"])?i18n($errors["surname"]):"" ?><input type="text" name="apellidos" value="<?=$user->getSurname()?>">
-
-				<?=i18n("Date Born")?>:<?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?><input type="text" id="datepicker" readonly="readonly" name="fechaNac" value="<?=$user->getDateBorn()?>">
-
-				<?=i18n("Email")?>:<?= isset($errors["email"])?i18n($errors["email"]):"" ?><input type="email" name="email" value="<?=$user->getEmail()?>">
-
-				<?=i18n("Telephone")?>:<?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?><input type="number" name="tel" value="<?=$user->getTlf()?>">
-
-				<?=i18n("DNI")?>:<?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?><input type="text" name="dni" value="<?=$user->getUsername()?>" readonly="readonly">
-
-				<?php if($user->getAdmin() == 1){ ?>
-				<?=i18n("Administrator")?><input type="checkbox" class="checkbox-style" name="administrador" value="1" checked="checked"> 
-				<?php }else{ ?>
-				<?=i18n("Administrator")?><input type="checkbox" class="checkbox-style" name="administrador" value="1">
-				<?php }if ($user->getCoach() == 1) { ?>
-				<?=i18n("Coach")?><input type="checkbox" class="checkbox-style" name="entrenador" value="1" checked="checked">
-				<?php }else{ ?>
-				<?=i18n("Coach")?><input type="checkbox" class="checkbox-style" name="entrenador" value="1">
-				<?php }if($user->getDeportist() == 1){ ?>
-				<?=i18n("Deportist")?><input type="checkbox" class="checkbox-style" name="deportista" value="1" checked="checked">
-				<?php }else{ ?>
-				<?=i18n("Deportist")?><input type="checkbox" class="checkbox-style" name="deportista" value="1">
-				<?php } ?>
-
-				<button type="submit" name="submit"><?=i18n("Send")?></button>
-
-			</form>
-		</div>
-	</div>
-</div>
+  <div class="form-group">
+    <div class="col-lg-offset-6 col-lg-6">
+      <button type="submit" name="submit" class="btn btn-default"><?=i18n("Send")?></button>
+    </div>
+  </div>
+</form>
