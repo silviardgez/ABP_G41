8ae342d618778ad69d632351e62b29c9faa11348
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:45:52 +0100

    Update editcurrent.php

--- view/users/editcurrent.php
+++ view/users/editcurrent.php
@@ -10,33 +10,66 @@ $errors = $view->getVariable("errors");
 $view->setVariable("title", "Edit Currentuser");
 ?>
 
-<div class="recuadro">
-	<div id="formulario">
-
-		<div class="home2">
-			<form action="index.php?controller=users&amp;action=editcurrent" method="POST">
-				<br><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?><input type="text" name="nombre" value="<?=$user->getName()?>"> 
-
-				<?=i18n("Surname")?>:<?= isset($errors["surname"])?i18n($errors["surname"]):"" ?><input type="text" name="apellidos" value="<?=$user->getSurname()?>">
-
-				<?=i18n("Date Born")?>:<?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?><input type="text" id="datepicker" readonly="readonly" name="fechaNac" value="<?=$user->getDateBorn()?>">
-
-				<?=i18n("Email")?>:<?= isset($errors["email"])?i18n($errors["email"]):"" ?><input type="email" name="email" value="<?=$user->getEmail()?>">
-
-				<?=i18n("Telephone")?>:<?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?><input type="number" name="tel" value="<?=$user->getTlf()?>">
-
-				<?=i18n("DNI")?>:<?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?><input type="text" name="dni" value="<?=$user->getUsername()?>" readonly="readonly">
-
-				<?=i18n("Password")?>:<?= isset($errors["oldpasswd"])?i18n($errors["oldpasswd"]):"" ?><input type="password" name="pass">
-
-				<?=i18n("New Password")?>:<?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?><input type="password" name="newpass">
-
-				<?=i18n("Repeat Password")?>:<input type="password" name="rpass">
-
-				<button type="button" onclick="history.back()"><?=i18n("Back")?></button>
-				<button type="submit" name="submit"><?=i18n("Send")?></button>
-
-			</form>
+<h1>Modificar Perfil</h1>
+<form action="index.php?controller=users&amp;action=editcurrent" method="POST" class="form-horizontal col-md-12" >
+  <br><div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" name="nombre" value="<?=$user->getName()?>">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Surname")?>:<?= isset($errors["surname"])?i18n($errors["surname"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" name="apellidos" value="<?=$user->getSurname()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Date Born")?>:<?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" id="fecha" name="fechaNac" value="<?=$user->getDateBorn()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Email")?>:<?= isset($errors["email"])?i18n($errors["email"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="email" name="email" value="<?=$user->getEmail()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("Telephone")?>:<?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?></label>
+    <div class="col-lg-6">
+      <input type="text" name="tel" value="<?=$user->getTlf()?>">
+    </div>
+  </div>
+	<div class="form-group">
+    <label class="col-lg-6 control-label"><?=i18n("DNI")?>:<?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?></label>
+    <div class="col-lg-6">
+    	<input type="text" name="dni" value="<?=$user->getUsername()?>" readonly="readonly">
+    </div>
+  </div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Password")?>:<?= isset($errors["oldpasswd"])?i18n($errors["oldpasswd"]):"" ?></label>
+		<div class="col-lg-6">
+			<input type="password" name="pass">
 		</div>
 	</div>
-</div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("New Password")?>:<?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?></label>
+		<div class="col-lg-6">
+			<input type="password" name="newpass">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Repeat Password")?>:</label>
+		<div class="col-lg-6">
+			<input type="password" name="rpass">
+		</div>
+	</div>
+  <div class="form-group">
+    <div class="col-lg-offset-6 col-lg-6">
+			<button type="button" onclick="history.back()"><?=i18n("Back")?></button>
+			<button type="submit" name="submit"><?=i18n("Send")?></button>
+    </div>
+  </div>
+</form>
