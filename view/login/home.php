753889ced28e78978ed5a24ce8cbac646594b631
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:55:49 +0100

    Update home.php

--- view/login/home.php
+++ view/login/home.php
@@ -12,10 +12,10 @@ $errors = $view->getVariable("errors");
 			<div class="col-md-4 col-sm-6 item">
 				<div class="box">
 					<i class="fa fa-envelope-o icon"></i>
-					<h3 class="name">Contacto</h3>
-					<p class="description">Pabellón Universitario</p>
-					<p class="description">Campus A Lonia (Ourense)</p>
-					<p class="description">Teléfono: 988 101 717</p>
+					<h3 class="name"><?=i18n("Contact")?></h3>
+					<p class="description"><?=i18n("University Pavilion")?></p>
+					<p class="description"><?=i18n("Campus A Lonia (Ourense)")?></p>
+					<p class="description"><?=i18n("Phone: 988 101 717")?></p>
 					<p><a class="description" href="http://ourense.gaiagd.com/">Web: Ourense.gaiagd.com</a></p>
 					<p><a class="description" href="https://www.facebook.com/aqa.orense">Facebook: AQUA Ourense</a></p>
 				</div>
@@ -23,20 +23,20 @@ $errors = $view->getVariable("errors");
 			<div class="col-md-4 col-sm-6 item">
 				<div class="box">
 					<i class="fa fa-university icon"></i>
-					<h3 class="name">Espacios Deportivos</h3>
-					<p class="description">En el Campus de Ourense las personas usuarias al Área de Bienestar, Salud y Deporte podrán utilizar una serie de instalaciones deportivas gestionadas por la UVigo:</p>
-					<p class="learn-more">Pabellón Polideportivo, Sala cardio-fitness,</p>
-					<p class="learn-more">Área de tenis de mesa, Pistas de tenis,</p>
-					<p class="learn-more">Campo de hierba artificial, Pista de Atletismo y AQUA.</p>
+					<h3 class="name"><?=i18n("Sport Spaces")?></h3>
+					<p class="description"><?=i18n("On the Campus of Ourense, users of the Area of ​​Well-being, Health and Sports may use a series of sports facilities managed by UVigo:")?></p>
+					<p class="learn-more"><?=i18n("Polideportivo Pavilion, Cardio-fitness room,")?></p>
+					<p class="learn-more"><?=i18n("Table tennis area, Tennis courts,")?></p>
+					<p class="learn-more"><?=i18n("Field of artificial grass, Track of Athletics and AQUA.")?></p>
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 item">
 				<div class="box">
 					<i class="fa fa-calendar icon"></i>
-					<h3 class="name">Horarios</h3>
-					<p class="description">Septiembre a Junio: De lunes a viernes de 08:30 a 23:30. Sábados y domingos de 10:00 a 14:00 y de 16:00 a 21:00.</p>
-					<p class="description">Julio, Semana Santa y Navidad: De lunes a viernes de 09:00 a 21:00. Sábados y domingos de 10:00 a 14:00 y de 16:00 a 21:00.</p>
-					<p class="description">Agosto: De lunes a viernes de 09:00 a 20:30. Sábados y domingos: Cerrado.</p>
+					<h3 class="name"><?=i18n("Schedules")?></h3>
+					<p class="description"><?=i18n("September to June: Monday to Friday from 08:30 to 23:30. Saturdays and Sundays from 10:00 a.m. to 2:00 p.m. and from 4:00 p.m. to 9:00 p.m.")?></p>
+					<p class="description"><?=i18n("July, Easter and Christmas: Monday to Friday from 9:00 a.m. to 9:00 p.m. Saturdays and Sundays from 10:00 a.m. to 2:00 p.m. and from 4:00 p.m. to 9:00 p.m.")?></p>
+					<p class="description"><?=i18n("August: Monday through Friday from 09:00 to 20:30. Saturdays and Sundays: Closed.")?></p>
 				</div>
 			</div>
 		</div>
