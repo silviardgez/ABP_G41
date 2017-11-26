a0fefbb4260752779bbeb7f80f4117282f95b213
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:42:12 +0100

    Update edit.php

--- view/exercises/edit.php
+++ view/exercises/edit.php
@@ -10,38 +10,54 @@ $errors = $view->getVariable("errors");
 $view->setVariable("title", "Edit Exercise");
 ?>
 
-<div class="recuadro">
-	<div id="formulario">
-
-		<div class="home2">
-			<form action="index.php?controller=exercises&amp;action=edit" method="POST">
-				<br><?= isset($errors["id"])?i18n($errors["id"]):"" ?><input type="hidden" name="id" value="<?=$exercise->getId()?>">  
-				<br><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?><input type="text" name="nombre" value="<?=$exercise->getName()?>">  
-
-				<?=i18n("Type")?>:
-				<select name="tipo">
-					<?php if($exercise->getType() == "CARDIO"){ ?>
-					<option value="CARDIO" selected="selected"><?=i18n("Cardio")?></option>
-					<option value="MUSCULAR"><?=i18n("Muscular")?></option>
-					<option value="ESTIRAMIENTO"><?=i18n("Stretch")?></option>
-					<?php }elseif ($exercise->getType() == "MUSCULAR") { ?>
-					<option value="CARDIO"><?=i18n("Cardio")?></option>
-					<option value="ESTIRAMIENTO"><?=i18n("Stretch")?></option>
-					<option value="MUSCULAR" selected="selected"><?=i18n("Muscular")?></option>
-					<?php }else{ ?>
-					<option value="CARDIO"><?=i18n("Cardio")?></option>
-					<option value="ESTIRAMIENTO" selected="selected"><?=i18n("Stretch")?></option>
-					<option value="MUSCULAR"><?=i18n("Muscular")?></option>
-					<?php } ?>
-				</select>
-
-				<?=i18n("Image")?>:<input type="file" name="imagen" value="<?=$exercise->getImage()?>"> 
-
-				<?=i18n("Video")?>:<input type="file" name="video" value="<?=$exercise->getVideo()?>">
-
-				<button type="submit" name="submit"><?=i18n("Send")?></button>
-
-			</form>
+<h1>Editar Ejercicio</h1>
+<form action="index.php?controller=exercises&amp;action=edit" method="POST" class="form-horizontal col-md-12" >
+	<br><div class="form-group">
+		<div class="col-lg-6">
+			<input type="hidden" name="id" value="<?=$exercise->getId()?>">
 		</div>
 	</div>
-</div>
+	<br><div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+		<div class="col-lg-7">
+			<input type="text" name="nombre" value="<?=$exercise->getName()?>">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Type")?>:</label>
+		<div class="col-lg-7">
+			<select name="tipo">
+				<?php if($exercise->getType() == "CARDIO"){ ?>
+				<option value="CARDIO" selected="selected"><?=i18n("Cardio")?></option>
+				<option value="MUSCULAR"><?=i18n("Muscular")?></option>
+				<option value="ESTIRAMIENTO"><?=i18n("Stretch")?></option>
+				<?php }elseif ($exercise->getType() == "MUSCULAR") { ?>
+				<option value="CARDIO"><?=i18n("Cardio")?></option>
+				<option value="ESTIRAMIENTO"><?=i18n("Stretch")?></option>
+				<option value="MUSCULAR" selected="selected"><?=i18n("Muscular")?></option>
+				<?php }else{ ?>
+				<option value="CARDIO"><?=i18n("Cardio")?></option>
+				<option value="ESTIRAMIENTO" selected="selected"><?=i18n("Stretch")?></option>
+				<option value="MUSCULAR"><?=i18n("Muscular")?></option>
+				<?php } ?>
+			</select>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Image")?>:</label>
+		<div class="col-lg-7">
+			<input type="file" name="imagen" value="<?=$exercise->getImage()?>">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-5 control-label"><?=i18n("Video")?>:</label>
+		<div class="col-lg-7">
+			<input type="file" name="video" value="<?=$exercise->getVideo()?>">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-lg-offset-5 col-lg-7s">
+			<button type="submit" name="submit"><?=i18n("Send")?></button>
+		</div>
+	</div>
+</form>
