5ea53cc3db7917d7729e173de6510466f430b4d8
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:41:30 +0100

    Update view.php

--- view/athletesstatistics/view.php
+++ view/athletesstatistics/view.php
@@ -22,33 +22,34 @@ $chart->setTitle("Monthly usage for www.example.com");
 $chart->render("libchart/demo/generated/demo1.png");*/
 ?>
 
-<div class="users">
-	<div class="margin">
-		<div class="home2">
-			<h1><?=i18n("Statistics")?></h1><br>
-			<?php foreach ($statistics as $statistic): ?>
-				Asiste a: <?= $statistic->getAsistenciaActividades(); ?> actividades<br/>
-				Está matriculado en: <?= $statistic->getMatriculas(); ?> actividades<br/>
-				Ha asistido: <?= $statistic->getAsistenciasTotales(); ?> veces al gimnasio<br/>
-				Está matriculado en el: <?= $statistic->getPorcentajeMatriculas(); ?> % de las actividades<br/>
-				Asiste al: <?= $statistic->getporcentajeAsistencias(); ?> % de las actividades en las que está matriculado<br/>
-			<?php endforeach; ?>
-		</div>
+<div class="col-md-12">
+	<div class="col-md-4"></div>
+	<div class="list-group col-md-4">
+	  <p class="list-group-item active tittle col-md-12">
+	    <?=i18n("Statistics")?>
+	  </p>
+		<?php foreach ($statistics as $statistic): ?>
+	  <p class="list-group-item col-md-12">Asiste a: <?= $statistic->getAsistenciaActividades(); ?> actividades</p>
+	  <p class="list-group-item col-md-12">Está matriculado en: <?= $statistic->getMatriculas(); ?> actividades</p>
+	  <p class="list-group-item col-md-12">Ha asistido: <?= $statistic->getAsistenciasTotales(); ?> veces al gimnasio</p>
+	  <p class="list-group-item col-md-12">Asiste al: <?= $statistic->getporcentajeAsistencias(); ?> % de las actividades en las que está matriculado</p>
+		<?php endforeach; ?>
 	</div>
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	<div class="col-md-4"></div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
