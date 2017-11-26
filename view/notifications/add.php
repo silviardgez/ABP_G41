6f06794fa7ab2b1476117334ea13d763c67bc66f
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-26 11:44:05 +0100

    Update add.php

--- view/notifications/add.php
+++ view/notifications/add.php
@@ -7,22 +7,29 @@ $dni = $view->getVariable("dni");
 $email = $view->getVariable("email");
 ?>
 
-<div class="recuadro">
-	<div id="formulario">
-
-		<div class="home2">
-			<form action="index.php?controller=notifications&amp;action=add" method="POST">
-				<br><input type="hidden" name="dni" value="<?= $dni ?>" readonly="readonly">  
- 
-				<?=i18n("Email")?>:<input type="text" name="email" value="<?= $email ?>" readonly="readonly">
-
-				<?=i18n("Subject")?>:<input type="text" name="subject"> 
-
-				<?=i18n("Message")?>:<input type="text" name="message">
-
-				<button type="submit" name="submit"><?=i18n("Send")?></button>
-
-			</form>
+<h1>Añadir Notificación Individual</h1>
+<form action="index.php?controller=notifications&amp;action=add" method="POST" class="form-horizontal col-md-12" >
+	<br><div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Email")?>:</label>
+		<div class="col-lg-6">
+			<input type="text" name="email" value="<?= $email ?>" readonly="readonly">
 		</div>
 	</div>
-</div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Subject")?>:</label>
+		<div class="col-lg-6">
+			<input type="text" name="subject">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-lg-6 control-label"><?=i18n("Message")?>:</label>
+		<div class="col-lg-6">
+			<textarea rows="3" name="message"></textarea>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-lg-offset-6 col-lg-6">
+			<button type="submit" name="submit" class="btn btn-default"><?=i18n("Send")?></button>
+		</div>
+	</div>
+</form>
