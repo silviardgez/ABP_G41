a7ec5cee900d1fc9ba295b1b7ad806c498347dcc
cibercereal <bcgonzalez4@esei.uvigo.es>
2017-11-29 17:27:35 +0100

    Add files via upload

--- view/exercises/view.php
+++ view/exercises/view.php
@@ -5,24 +5,28 @@ $view = ViewManager::getInstance();
 $exercise = $view->getVariable("exercise");
 $view->setVariable("title", "View exercise");
 ?>
+<div>
+	<h1 id="bigger-size" class="stroke"><?=i18n("View Exercice")?></h1>
+	<br>
+</div>
 
-<h1><?=i18n("View Exercice")?></h1>
-<form class="form-horizontal col-md-12" >
+<div id="edit-view" class="center-block col-xs-6 col-lg-6">
+<form class="center-block form-horizontal" >
 	<br><div class="form-group">
-		<label class="col-lg-5 control-label"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
 		<div class="col-lg-7">
 			<input type="text" name="nombre" value="<?=$exercise->getName()?>" readonly="readonly">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="col-lg-5 control-label"><?=i18n("Type")?>:</label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Type")?>:</label>
 		<div class="col-lg-7">
 			<input type="text" name="tipo" value="<?=$exercise->getType()?>" readonly="readonly">
 		</div>
 	</div>
 	<div class="form-group">
 		<?php if($exercise->getImage() != NULL && $exercise->getImage() != ""){ ?>
-			<label class="col-lg-5 control-label"><?=i18n("Image")?>:</label>
+			<label class="control-label text-size text-muted col-sm-4"><?=i18n("Image")?>:</label>
 			<div class="col-lg-7">
 				<img src="<?=$exercise->getImage()?>" alt="<?=i18n("Image not found")?>" />
 			</div>
@@ -30,15 +34,16 @@ $view->setVariable("title", "View exercise");
 	</div>
 	<div class="form-group">
 		<?php if($exercise->getVideo() != NULL && $exercise->getVideo() != ""){ ?>
-		<label class="col-lg-5 control-label"><?=i18n("Video")?>:</label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Video")?>:</label>
 		<div class="col-lg-7">
 			<iframe width="560" height="315" src="<?=$exercise->getVideo()?>" controls autoplay frameborder="0" allowfullscreen></iframe>
 		</div>
 		<?php }?>
 	</div>
 	<div class="form-group">
-		<div class="col-lg-offset-6 col-lg-6">
-			<button type="button" onclick="history.back()"><?=i18n("OK")?></button>
+		<div class="col-sm-12">
+			<button type="button" id="btn-styles" onclick="history.back()" class="btn btn-success btn-lg"><?=i18n("Ok")?></button>
 		</div>
 	</div>
 </form>
+</div>
