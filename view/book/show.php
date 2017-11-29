27c481ffcf19e730d5b2bc65d42317b82c964be8
arrodri <33467522+arrodri@users.noreply.github.com>
2017-11-29 02:46:32 +0100

    Update show.php

--- view/book/show.php
+++ view/book/show.php
@@ -4,26 +4,83 @@ require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
 //$view->setLayout("welcome");
 
-$users = $view->getVariable("users");
+$books = $view->getVariable("books");
+$names = $view->getVariable("names");
 $view->setVariable("title", "Bookings");
+$activities = $view->getVariable("activities");
 ?>
 
 
-<section class="pagecontent">
-	<div class="users">
-		<div class="margin">
-			<div class="home2">
-				<h1><?=i18n("Users")?></h1><br>
-				<?php foreach ($users as $user): ?>
+<!-- if usuario = admin or coach-->
+	<div class="container">
+		<div class="table-responsive col-md-6">
+			<table class="table">
+				<thead><tr>
+					<th class="tittle" colspan="5" colspan="5"><?=i18n("Bookings confirmed")?></th>
+				</tr>
+					<tr class="active">
+						<th><?=i18n("Name user")?></th>
+						<th><?=i18n("Name activity")?></th>
+						<th><?=i18n("Date of reservation")?></th>
+						<th><?=i18n("Hour of reservation")?></th>
+						<th><?=i18n("Confirmed")?></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					foreach ($books as $book):?>
+						<?php
+						if ($book['confirmed'] == 1):
+							?>
+							<tr class = "success">
 
-            <?= htmlentities($user->getName()); echo ' '.htmlentities($user->getSurname()) ?>
-						<div class="icons">
-            <a href="index.php?controller=book&amp;action=viewUser"><i class="fa fa-eye"></i></a>
-					</div>
+								<td><?= $book['userName'] ?></td> <!-- Devuelve el nombre del usuario-->
+								<td><?= $book['actName'] ?></td> <!-- Devuelve el nombre de la actividad-->
+								<td><?= $book['dateBook'] ?></td>
+								<td><?= $book['hour'] ?></td>
+								<td>
+									<input disabled type="checkbox" name="confirmed" value="<?= $book['confirmed'] ?>" checked>
+										Confirmed
+									<a href="index.php?controller=book&action=changeConfirmedStatus&idAct=<?= $book['idAct'] ?>&idAthl=<?= $book['idAthl'] ?>&status=0">Decline</a>
 
-			<?php endforeach; ?>
+								</td>
+							</tr>
+					<?php endif; ?>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+
+		<div class="table-responsive col-md-6">
+			<table class="table">
+				<thead><tr>
+					<th class="tittle" colspan="5"><?=i18n("Bookings not confirmed")?></th>
+				</tr>
+					<tr class="active">
+						<th><?=i18n("Name user")?></th>
+						<th><?=i18n("Name activity")?></th>
+						<th><?=i18n("Date of reservation")?></th>
+						<th><?=i18n("Hour of reservation")?></th>
+						<th><?=i18n("Confirmed")?></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($books as $book):?>
+						<?php if ($book['confirmed']==0): ?>
+							<tr class = "success">
+								<td><?= $book['userName'] ?></td> <!-- Devuelve el nombre del usuario-->
+								<td><?= $book['actName'] ?></td> <!-- Devuelve el nombre de la actividad-->
+								<td><?= $book['dateBook'] ?></td>
+								<td><?= $book['hour'] ?></td>
+								<td>
+									<input disabled type="checkbox" name="confirmed" value="<?= $book['confirmed'] ?>">
+									Not confirmed
+									<a href="index.php?controller=book&action=changeConfirmedStatus&idAct=<?= $book['idAct'] ?>&idAthl=<?= $book['idAthl'] ?>&status=1">Confirm</a>
+								</td>
+							</tr>
+					<?php endif ?>
+					<?php endforeach ?>
+				</tbody>
+			</table>
 		</div>
 	</div>
-</div>
-
-</section>
