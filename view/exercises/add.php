e504210e9636eb54ce17168d7f481baa6918d6d3
cibercereal <bcgonzalez4@esei.uvigo.es>
2018-01-05 23:21:17 +0100

    Update add.php
    añadido boton de atras

--- view/exercises/add.php
+++ view/exercises/add.php
@@ -49,7 +49,10 @@ $errors = $view->getVariable("errors");
 		</div>
 	</div>
 	<div class="form-group">
-		<div class="col-sm-12">
+		<div class="col-sm-6">
+      <button type="button" id="btn-styles" onclick="history.back()" class="btn btn-warning btn-lg"><?=i18n("Back")?></button>
+    </div>
+		<div class="col-sm-6">
 			<button id="btn-styles" type="submit" name="submit" class="btn btn-success btn-lg"><?=i18n("Send")?></button>
 		</div>
 	</div>
