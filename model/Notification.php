c36000e6ba2f32a39a6357ad05328d0b6feb8d83
cibercereal <bcgonzalez4@esei.uvigo.es>
2018-01-08 18:25:32 +0100

    Update Notification.php

--- model/Notification.php
+++ model/Notification.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__."/../core/ValidationException.php");
-require_once(__DIR__."/../model/ExerciseMapper.php");
+require_once(__DIR__."/../model/NotificationMapper.php");
 
 class Notification{
 
