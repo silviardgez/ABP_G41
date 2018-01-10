f58f82393d17cb0ece268f8145e8c6e017f8bddd
cibercereal <bcgonzalez4@esei.uvigo.es>
2018-01-10 17:36:54 +0100

    Update add.php

--- view/assistance/add.php
+++ view/assistance/add.php
@@ -6,21 +6,19 @@ $view = ViewManager::getInstance();
 
 $assistants = $view->getVariable("assistants");
 $assistance = $view->getVariable("assistance");
+$errors = $view->getVariable ( "errors" );
 
 $view->setVariable("title", "Add Assistance");
 ?>
 
 <div>
-	<h1 id="bigger-size" class="stroke"><?=i18n("Assistance")?></h1>
+	<h1 id="bigger-size" class="stroke"><?=i18n("Add Assistance")?></h1>
 	<br>
 </div>
 
-<div class="col-md-4"></div>
-	<div class="row features margin-rows">
-		<div class="col-md-5 col-sm-6 item">
-			<div class="exercise-tables-background">
-		<h1 id="font-title"><?=i18n("Add assistance")?></h1>
-		<br>
+<div id="edit-view" class="center-block col-xs-6 col-lg-6">
+<br>
+<form action="index.php?controller=assistance&amp;action=add&amp;id_act=<?= $assistance->getActivityid(); ?>" method="POST" class="center-block form-horizontal">
 			<table id="table-margin" class="table">
 				<thead>
 							<tr class="active">
@@ -29,40 +27,49 @@ $view->setVariable("title", "Add Assistance");
 					</thead>
 					<tbody>
 						<?php foreach ($assistants as $assistant): ?>
-							<form action="index.php?controller=assistance&amp;action=add&amp;id_act=<?= $assistance->getActivityid(); ?>" method="POST">
 								<tr>
 									<input type="hidden" name="id_act" value="<?= $assistance->getActivityid(); ?>">
 
 									<td>
-										<?= $assistant->getDni(); ?> 
+										<?= $assistant->getDni(); ?>
 										<label>
 											<a href="index.php?controller=users&amp;action=view&amp;dni=<?= $assistant->getDni(); ?>">
-												<i class="fa fa-search col-md-3"></i> 
+												<i class="fa fa-search col-md-3"></i>
 											</a>
 										</label>
 										<input type="checkbox" name="asistentes[]" value="<?= $assistant->getDni(); ?>">
-										
+
 									</td>
-									
-									
+
+
 								</tr><?php endforeach; ?>
-								
-						
+
 					</tbody>
-				</table>
-				
-				<br/>
-				<b><?=i18n("Date")?></b> <input type="date" id="" name="fecha"><br/><br/>
-				<b><?=i18n("Time")?></b> <input type="time" id="hora" name="hora" value="<?= $assistance->getTime(); ?>"><br/><br/>
-				<button id="button2" type="submit" name="submit"><?=i18n("Add")?></button>			
-							</form>
-				
-		</div>
-	</div>
-	</div>
-	<div class="form-group">
-		<div class="col-lg-offset-6 col-lg-6">
-			<button type="button" onclick="history.back()"><?=i18n("OK")?></button>
-		</div>
-	</div>
-<div class="col-md-4"></div>
+			</table>
+
+				<br>
+				<div class="form-group">
+					<label class="control-label text-size text-muted col-sm-4"><?=i18n("Date")?> <?= isset($errors["dateassistance"])?i18n($errors["dateassistance"]):"" ?></label>
+					<div class="col-lg-6">
+					 <input class="form-control" type="date" id="" name="fecha"><br/><br/>
+				 	</div>
+				</div>
+
+			 <div class="form-group">
+					<label class="control-label text-size text-muted col-sm-4"><?=i18n("Time")?><?= isset($errors["timeassistance"])?i18n($errors["timeassistance"]):"" ?></label>
+					<div class="col-lg-6">
+						<input class="form-control" type="time" id="hora" name="hora" value="<?= $assistance->getTime(); ?>"><br/><br/>
+					</div>
+			 </div>
+
+				<div class="form-group">
+					<div class="col-sm-6">
+						<button id="btn-styles" type="button" onclick="history.back()" class="btn btn-warning btn-lg"><?=i18n("Back")?></button>
+					</div>
+					<div class="col-sm-6">
+						<button id="btn-styles" type="submit" name="submit" class="btn btn-success btn-lg"><?=i18n("Add")?></button>
+					</div>
+				</div>
+			</form>
+
+</div>
