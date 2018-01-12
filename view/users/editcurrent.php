8cb54a95cbadb08b9946262a3f689afe68dcd13d
cibercereal <bcgonzalez4@esei.uvigo.es>
2018-01-12 01:00:27 +0100

    Update editcurrent.php

--- view/users/editcurrent.php
+++ view/users/editcurrent.php
@@ -18,49 +18,49 @@ $view->setVariable("title", "Edit Currentuser");
 <div id="edit-view" class="center-block col-xs-6 col-lg-4">
 <form action="index.php?controller=users&amp;action=editcurrent" method="POST" class="center-block form-horizontal">
   <br><div class="form-group">
-    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Name")?>:<b class="aviso-vacio"><?= isset($errors["name"])?i18n($errors["name"]):"" ?></b></label>
     <div class="col-lg-6">
       <input class="form-control" type="text" name="nombre" value="<?=$user->getName()?>">
     </div>
   </div>
   <div class="form-group">
-    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Surname")?>:<?= isset($errors["surname"])?i18n($errors["surname"]):"" ?></label>
+    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Surname")?>:<b class="aviso-vacio"><?= isset($errors["surname"])?i18n($errors["surname"]):"" ?></b></label>
     <div class="col-lg-6">
       <input class="form-control" type="text" name="apellidos" value="<?=$user->getSurname()?>">
     </div>
   </div>
 	<div class="form-group">
-    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Date Born")?>:<?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?></label>
+    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Date Born")?>:<b class="aviso-vacio"><?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?></b></label>
     <div class="col-lg-6">
       <input class="form-control" type="date" id="date" name="fechaNac" value="<?=$user->getDateBorn()?>">
     </div>
   </div>
 	<div class="form-group">
-    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Email")?>:<?= isset($errors["email"])?i18n($errors["email"]):"" ?></label>
+    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Email")?>:<b class="aviso-vacio"><?= isset($errors["email"])?i18n($errors["email"]):"" ?></b></label>
     <div class="col-lg-6">
       <input class="form-control" type="email" name="email" value="<?=$user->getEmail()?>">
     </div>
   </div>
 	<div class="form-group">
-    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Telephone")?>:<?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?></label>
+    <label class="control-label text-size text-muted col-sm-4"><?=i18n("Telephone")?>:<b class="aviso-vacio"><?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?></b></label>
     <div class="col-lg-6">
       <input class="form-control" type="text" name="tel" value="<?=$user->getTlf()?>">
     </div>
   </div>
 	<div class="form-group">
-    <label class="control-label text-size text-muted col-sm-4"><?=i18n("DNI")?>:<?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?></label>
+    <label class="control-label text-size text-muted col-sm-4"><?=i18n("DNI")?>:<b class="aviso-vacio"><?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?></b></label>
     <div class="col-lg-6">
     	<input class="form-control" type="text" name="dni" value="<?=$user->getUsername()?>" readonly="readonly">
     </div>
   </div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Password")?>:<?= isset($errors["oldpasswd"])?i18n($errors["oldpasswd"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Password")?>:<b class="aviso-vacio"><?= isset($errors["oldpasswd"])?i18n($errors["oldpasswd"]):"" ?></b></label>
 		<div class="col-lg-6">
 			<input class="form-control" type="password" name="pass">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("New Password")?>:<?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("New Password")?>:<b class="aviso-vacio"><?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?></b></label>
 		<div class="col-lg-6">
 			<input class="form-control" type="password" name="newpass">
 		</div>
