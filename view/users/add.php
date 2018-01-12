9d226cd164423ba292c5c0c46963dc838c3881bd
cibercereal <bcgonzalez4@esei.uvigo.es>
2018-01-12 01:00:51 +0100

    Update add.php

--- view/users/add.php
+++ view/users/add.php
@@ -17,45 +17,45 @@ $errors = $view->getVariable ( "errors" );
 	action="index.php?controller=users&amp;action=add" method="POST">
 	<br>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Name")?>:<?= isset($errors["name"])?i18n($errors["name"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Name")?>:<b class="aviso-vacio"><?= isset($errors["name"])?i18n($errors["name"]):"" ?></b></label>
 		<div class="col-sm-8">
 			<input class="form-control" type="text" name="nombre">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Surname")?>:<?= isset($errors["surname"])?i18n($errors["surname"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Surname")?>:<b class="aviso-vacio"><?= isset($errors["surname"])?i18n($errors["surname"]):"" ?><b></label>
 		<div class="col-sm-8">
 			<input class="form-control" type="text" name="apellidos">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Date Born")?>:<?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Date Born")?>:<b class="aviso-vacio"><?= isset($errors["dateborn"])?i18n($errors["dateborn"]):"" ?></b></label>
 		<div class="col-sm-8">
 			<input type="date" id="date" class="form-control"
 			name="fechaNac">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Email")?>:<?= isset($errors["email"])?i18n($errors["email"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Email")?>:<b class="aviso-vacio"><?= isset($errors["email"])?i18n($errors["email"]):"" ?></b></label>
 		<div class="col-sm-8">
 			<input class="form-control" type="email" name="email">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Telephone")?>:<?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Telephone")?>:<b class="aviso-vacio"><?= isset($errors["tlf"])?i18n($errors["tlf"]):"" ?></b></label>
 		<div class="col-sm-8">
 			<input class="form-control" type="text" name="tel"
 			value="<?=$user->getTlf()?>">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("DNI")?>:<?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("DNI")?>:<b class="aviso-vacio"><?= isset($errors["DNI"])?i18n($errors["DNI"]):"" ?></b></label>
 		<div class="col-sm-8">
 			<input class="form-control" type="text" name="dni">
 		</div>
 	</div>
 	<div class="form-group">
-		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Password")?>:<?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?></label>
+		<label class="control-label text-size text-muted col-sm-4"><?=i18n("Password")?>:<b class="aviso-vacio"><?= isset($errors["passwd"])?i18n($errors["passwd"]):"" ?></b></label>
 		<div class="col-sm-8">
 			<input class="form-control" type="password" name="pass">
 		</div>
@@ -87,7 +87,7 @@ $errors = $view->getVariable ( "errors" );
 	<div class="form-group">
 		<div class="col-sm-12">
 			<div class="checkbox">
-				<?= isset($errors["athlete"])?i18n($errors["athlete"]):"" ?>
+				<b class="aviso-vacio"><?= isset($errors["athlete"])?i18n($errors["athlete"]):"" ?></b>
 				<label class="control-label text-size text-muted col-sm-8">
 					<input type="checkbox" name="deportista_tdu" value="1"> <?=i18n("Athlete"); echo " TDU"?>
 				</label>
